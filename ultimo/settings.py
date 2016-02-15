7b2389fcbd0a61411f2a0758901cb18468884b9f
escuderot <escuderotreeintelligence@gmail.com>
2016-02-15 17:53:39 -0300

    Update settings.py

--- ultimo/settings.py
+++ ultimo/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
+
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+
+    )
